9beaa9e2f7033968833e652febf86aceb0174611
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2020-02-08 14:19:53 -0500

    Finished Boba Buds and updated SCSS styles

--- src/templates/blog_template.js
+++ src/templates/blog_template.js
@@ -6,6 +6,10 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { INLINES, BLOCKS } from "@contentful/rich-text-types"
 import Head from "../components/head"
 
+import PropTypes from "prop-types"
+import ReactDOM from "react-dom"
+import Gallery from "react-grid-gallery"
+
 import blogTemplateStyles from "./blog_template.module.scss"
 
 const website_url = "https://jaydenhsiao.me"
@@ -20,24 +24,85 @@ export const query = graphql`
     }
   }
 `
+class Grid extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      images: this.props.images,
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "block",
+          minHeight: "1px",
+          width: "100%",
+          overflow: "auto",
+        }}
+      >
+        <Gallery
+          images={this.state.images}
+          enableLightbox={true}
+          enableImageSelection={false}
+        />
+      </div>
+    )
+  }
+}
+
+Grid.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      thumbnail: PropTypes.string.isRequired,
+      srcset: PropTypes.array,
+      caption: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+      thumbnailWidth: PropTypes.number.isRequired,
+      thumbnailHeight: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+}
 
 const Blog = props => {
   const options = {
     renderNode: {
       [BLOCKS.EMBEDDED_ASSET]: node => {
-        const alt = node.data.target.fields.description["en-US"]
-        const url = node.data.target.fields.file["en-US"].url
-        return (
-          <div className={blogTemplateStyles.imageContainer}>
-            <img alt={alt} src={url} />
-            <div className={blogTemplateStyles.captionContainer}>
-              {" "}
-              <p className={blogTemplateStyles.caption}>
-                {node.data.target.fields.description["en-US"]}
-              </p>
+        var bool = true
+        const words = node.data.target.fields.title["en-US"].split(" ")
+        if (words[words.length - 1] === "GIF") {
+          bool = true
+          const alt = node.data.target.fields.description["en-US"]
+          const url = node.data.target.fields.file["en-US"].url
+          return (
+            <div className={blogTemplateStyles.gifContainer}>
+              <img alt={alt} src={url} className={blogTemplateStyles.gif} />
+              <div className={blogTemplateStyles.captionContainer}>
+                {" "}
+                <p className={blogTemplateStyles.caption}>
+                  {node.data.target.fields.description["en-US"]}
+                </p>
+              </div>
             </div>
-          </div>
-        )
+          )
+        } else {
+          bool = false
+          const alt = node.data.target.fields.description["en-US"]
+          const url = node.data.target.fields.file["en-US"].url
+          return (
+            <div className={blogTemplateStyles.imageContainer}>
+              <img alt={alt} src={url} />
+              <div className={blogTemplateStyles.captionContainer}>
+                {" "}
+                <p className={blogTemplateStyles.caption}>
+                  {node.data.target.fields.description["en-US"]}
+                </p>
+              </div>
+            </div>
+          )
+        }
       },
       [INLINES.HYPERLINK]: node => {
         return (
