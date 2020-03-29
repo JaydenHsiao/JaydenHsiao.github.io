c9e11216f25403bce7591f4a74f6e1b74c5e7d16
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2020-03-28 21:14:19 -0400

    changed resume and added zoom on image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import Typing from "react-typing-animation"
+import { Fade } from "react-slideshow-image"
 import Slideshow from "../components/slideshow"
 import ProjectPage from "../components/project"
 import AdditionalProjectPage from "../components/addproject"
@@ -9,40 +10,48 @@ import Layout from "../components/layout"
 import Head from "../components/head"
 import Description from "../components/description"
 import Chat from "../components/chat"
-
-const IndexPage = () => {
-  return (
-    <Layout>
-      <Head title="Home" />
-      <h1 style={{ margin: "25vh 0 5vh 0" }}>
-        Hello, I'm Jayden - a <br />
-        <Typing loop={true}>
-          <div style={{ color: "#4895EA" }}>
-            <span>Product Designer</span>
-            <Typing.Delay ms={800} />
-            <Typing.Backspace count={17} />
-            <span>Photographer</span>
-            <Typing.Delay ms={1600} />
-            <Typing.Backspace count={13} />
-            <span>Pianist</span>
-            <Typing.Delay ms={2600} />
-            <Typing.Backspace count={8} />
-            <span>Acapella Enthusiast</span>
-            <Typing.Delay ms={1100} />
-            <Typing.Backspace count={20} />
-            <span>Friend</span>
-            <Typing.Delay ms={2700} />
-            <Typing.Backspace count={7} />
-          </div>
-        </Typing>
-        <Slideshow />
-      </h1>
-      <Description />
-      <Chat />
-      <ProjectPage />
-      <AdditionalProjectPage />
-    </Layout>
-  )
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.slideRef = React.createRef()
+  }
+  onBeforeType = () => {
+    this.slideRef.current.goNext()
+  }
+  render() {
+    return (
+      <Layout>
+        <Head title="Home" />
+        <h1 style={{ margin: "25vh 0 5vh 0" }}>
+          Hello, I'm Jayden - a <br />
+          <Typing loop={true} ref={this.slideRef}>
+            <div style={{ color: "#4895EA" }}>
+              <span>Product Designer</span>
+              <Typing.Delay ms={800} />
+              <Typing.Backspace count={17} />
+              <span>Photographer</span>
+              <Typing.Delay ms={1600} />
+              <Typing.Backspace count={13} />
+              <span>Pianist</span>
+              <Typing.Delay ms={2600} />
+              <Typing.Backspace count={8} />
+              <span>Acapella Enthusiast</span>
+              <Typing.Delay ms={1100} />
+              <Typing.Backspace count={20} />
+              <span>Friend</span>
+              <Typing.Delay ms={2700} />
+              <Typing.Backspace count={7} />
+            </div>
+          </Typing>
+          <Slideshow />
+        </h1>
+        <Description />
+        <Chat />
+        <ProjectPage />
+        <AdditionalProjectPage />
+      </Layout>
+    )
+  }
 }
 
 export default IndexPage
